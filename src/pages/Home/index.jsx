51ff98bb68b169c6026e7b375e587eef2c0175c1
Bruno <brunoalmeida0805@gmail.com>
2023-04-06 20:50:44 -0300

    criação da Home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,27 @@
+import {Container, Brand, Menu, Search, Content, NewNote} from "./style";
+
+import { Header } from "../../components/Header";
+
+export function Home () {
+  return(
+    <Container>
+      <Brand>
+
+      </Brand>
+
+      <Header />
+      <Menu>
+
+      </Menu>
+      <Search>
+
+      </Search>
+      <Content>
+
+      </Content>
+      <NewNote>
+
+      </NewNote>
+    </Container>
+  )
+}
